3b8c0e7ef5b64c3361e4b87b2bcc4c74a5d885cb
Alina Tugui <alina.tugui@stud.ubbcluj.ro>
2025-02-05 23:28:14 +0200

    add tutorial

---
+++ src/main/java/com/staxrt/tutorial/exception/AuthenticationException.java
@@ -0,0 +1,20 @@
+package com.staxrt.tutorial.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @Project spring-boot-rest-api-auth-jwt-tutorial
+ */
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+public class AuthenticationException extends Exception {
+
+    /**
+     * Instantiates a new Resource not found exception.
+     *
+     * @param message the message
+     */
+    public AuthenticationException(String message) {
+        super(message);
+    }
+}
